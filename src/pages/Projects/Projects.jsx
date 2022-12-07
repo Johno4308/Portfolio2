80239049567297904780936d3860be18eee30609
Johno4308 <67054611+Johno4308@users.noreply.github.com>
2022-12-07 12:28:27 +0200

    Project Page Progress

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaGithub, FaCoffee } from "react-icons/fa";
 import classes from "./Projects.module.scss";
-import Button from "./../../UI/Button/Button";
 import coverIMG1 from "./../../assets/Background1.png";
 import coverIMG2 from "./../../assets/Background2.png";
 import coverIMG3 from "./../../assets/Background3.png";
@@ -28,6 +27,9 @@ const Projects = () => {
           <span className={classes.hero__content__tagline}>
             A guide to making great different coffees.
           </span>
+          <span className={classes.hero__content__codes}>
+            | React | SCSS | REACT-THREE-FIBER | GSAP |
+          </span>
           <div className={classes.hero__content__item}>
             <img
               className={classes.hero__content__item__img}
@@ -52,12 +54,33 @@ const Projects = () => {
           </div>
 
           <p className={classes.hero__content__description}>
-            Built with react and scss. Purpose of this project was to practise
-            breakpoints and setting up the site to be mobile reponsive. Each of
+            Built with React and Scss. Purpose of this project was to practise
+            breakpoints and setting up the site to be mobile responsive. Each of
             the 18 coffees was stored as an object which I then mapped over to
-            create both the Nav list and coffee instruction component.
+            create both the Nav list and coffee instruction component. Extra the
+            mug in the first image is a 3D model that rotates and is interactive
+            using React-Three-Fiber.
           </p>
+
           <div className={classes.hero__content__cta}>
+            <a
+              href="https://caffiend.netlify.app/"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaCoffee className={classes.hero__content__cta__item__icon} />}
+              Website
+            </a>
+
+            <a
+              href="https://github.com/Johno4308/Caffiend"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaGithub className={classes.hero__content__cta__item__icon} />}
+              GitHub
+            </a>
+          </div>
+
+          {/* <div className={classes.hero__content__cta}>
             <Button outline>
               {<FaCoffee className={classes.hero__content__cta__icon} />}Website
             </Button>
@@ -67,7 +90,7 @@ const Projects = () => {
                 Github
               </Button>
             </a>
-          </div>
+          </div> */}
         </div>
         <div className={classes.hero__content}>
           <p>
