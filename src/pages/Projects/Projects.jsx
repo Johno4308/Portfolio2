7b332aea3e4695ee7ace94acfe7d0edc2479b5d4
Johno4308 <67054611+Johno4308@users.noreply.github.com>
2022-12-07 11:32:33 +0200

    Projects Page Progress

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { FaGithub, FaCoffee } from "react-icons/fa";
 import classes from "./Projects.module.scss";
+import Button from "./../../UI/Button/Button";
 import coverIMG1 from "./../../assets/Background1.png";
 import coverIMG2 from "./../../assets/Background2.png";
 import coverIMG3 from "./../../assets/Background3.png";
 import coverIMG4 from "./../../assets/Background4.png";
 import coverIMG5 from "./../../assets/Background5.png";
+import CaffiendProj1IMG from "./../../assets/caffiendProj1.jpg";
+import CaffiendProj2IMG from "./../../assets/caffiendProj2.jpg";
+import CaffiendProj3IMG from "./../../assets/caffiendProj3.jpg";
+import CaffiendProj4IMG from "./../../assets/caffiendProj4.jpg";
 const images = [coverIMG1, coverIMG2, coverIMG3, coverIMG4, coverIMG5];
 
 const Projects = () => {
@@ -16,12 +22,52 @@ const Projects = () => {
         <div className={classes.hero__content}>
           <h1 className={classes.hero__content__title}>Projects</h1>
         </div>
+        {/*       CAFFIEND            */}
         <div className={classes.hero__content}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            voluptas ipsam molestiae iste fuga perferendis libero fugit
-            accusantium soluta quidem?
+          <h1 className={classes.hero__content__title}>Caffiend</h1>
+          <span className={classes.hero__content__tagline}>
+            A guide to making great different coffees.
+          </span>
+          <div className={classes.hero__content__item}>
+            <img
+              className={classes.hero__content__item__img}
+              src={CaffiendProj1IMG}
+              width="200px"
+            />
+            <img
+              className={classes.hero__content__item__img}
+              src={CaffiendProj2IMG}
+              width="200px"
+            />
+            <img
+              className={classes.hero__content__item__img}
+              src={CaffiendProj3IMG}
+              width="200px"
+            />
+            <img
+              className={classes.hero__content__item__img}
+              src={CaffiendProj4IMG}
+              width="200px"
+            />
+          </div>
+
+          <p className={classes.hero__content__description}>
+            Built with react and scss. Purpose of this project was to practise
+            breakpoints and setting up the site to be mobile reponsive. Each of
+            the 18 coffees was stored as an object which I then mapped over to
+            create both the Nav list and coffee instruction component.
           </p>
+          <div className={classes.hero__content__cta}>
+            <Button outline>
+              {<FaCoffee className={classes.hero__content__cta__icon} />}Website
+            </Button>
+            <a href="https://github.com/Johno4308/Caffiend">
+              <Button>
+                {<FaGithub className={classes.hero__content__cta__icon} />}
+                Github
+              </Button>
+            </a>
+          </div>
         </div>
         <div className={classes.hero__content}>
           <p>
