1beeed2befb38aa6b78431c77cfa9e2e80770784
Johno4308 <67054611+Johno4308@users.noreply.github.com>
2022-12-07 18:21:03 +0200

    Projects page completed

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -10,6 +10,13 @@ import CaffiendProj1IMG from "./../../assets/caffiendProj1.jpg";
 import CaffiendProj2IMG from "./../../assets/caffiendProj2.jpg";
 import CaffiendProj3IMG from "./../../assets/caffiendProj3.jpg";
 import CaffiendProj4IMG from "./../../assets/caffiendProj4.jpg";
+import MemeProj1IMG from "./../../assets/memeProj1.jpg";
+import VillageProj1IMG from "./../../assets/villageProj1.jpg";
+import ContainerProj1IMG from "./../../assets/containerProj1.jpg";
+import ContainerProj2IMG from "./../../assets/containerProj2.jpg";
+import GuesthouseProj1IMG from "./../../assets/guesthouseProj1.jpg";
+import GuesthouseProj2IMG from "./../../assets/guesthouseProj2.jpg";
+import CalculatorProj1IMG from "./../../assets/calculatorProj1.png";
 const images = [coverIMG1, coverIMG2, coverIMG3, coverIMG4, coverIMG5];
 
 const Projects = () => {
@@ -79,53 +86,182 @@ const Projects = () => {
               GitHub
             </a>
           </div>
+        </div>
 
-          {/* <div className={classes.hero__content__cta}>
-            <Button outline>
-              {<FaCoffee className={classes.hero__content__cta__icon} />}Website
-            </Button>
-            <a href="https://github.com/Johno4308/Caffiend">
-              <Button>
-                {<FaGithub className={classes.hero__content__cta__icon} />}
-                Github
-              </Button>
+        {/*       MEMEAPI           */}
+        <div className={classes.hero__content}>
+          <h1 className={classes.hero__content__title}>MEME API</h1>
+          <span className={classes.hero__content__tagline}>
+            A simply meme making tool.
+          </span>
+          <span className={classes.hero__content__codes}>| React | CSS |</span>
+          <div className={classes.hero__content__item}>
+            <img
+              className={classes.hero__content__item__img}
+              src={MemeProj1IMG}
+              width="80%"
+            />
+          </div>
+          <p className={classes.hero__content__description}>
+            Built with React and CSS. Its a simple application that recieves 100
+            of most popular MEME images from an API and allows the user to add
+            text to the top and bottom of the image in classic MEME font.
+          </p>
+          <div className={classes.hero__content__cta}>
+            <a
+              href="https://github.com/Johno4308/MemeAPI"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaGithub className={classes.hero__content__cta__item__icon} />}
+              GitHub
             </a>
-          </div> */}
+          </div>
+        </div>
+        {/*       VILLAGE           */}
+        <div className={classes.hero__content}>
+          <h1 className={classes.hero__content__title}>VILLAGE</h1>
+          <span className={classes.hero__content__tagline}>
+            UNIVERSITY COMPUTER GRAPHICS PROJECT - VILLAGE
+          </span>
+          <span className={classes.hero__content__codes}>
+            | HTML | CSS | THREEJS |
+          </span>
+          <div className={classes.hero__content__item}>
+            <img
+              className={classes.hero__content__item__img}
+              src={VillageProj1IMG}
+              width="80%"
+            />
+          </div>
+          <p className={classes.hero__content__description}>
+            Built with THREEjs. University requirements were to have a hut, fire
+            place and igloo (don't ask why). With a focus on functionality
+            between HTML and JS over and above textures, objects and lighting.
+            Some of the button functionality was to, change scene direction,
+            change scene speed, toggle between day and night time, zoom in and
+            zoom out.
+          </p>
+          <div className={classes.hero__content__cta}>
+            <a
+              href="https://github.com/Johno4308/Village"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaGithub className={classes.hero__content__cta__item__icon} />}
+              GitHub
+            </a>
+          </div>
+        </div>
+        {/*       CONTAINER           */}
+        <div className={classes.hero__content}>
+          <h1 className={classes.hero__content__title}>CONTAINER</h1>
+          <span className={classes.hero__content__tagline}>
+            An application to keep track of the heavy lifting
+          </span>
+          <span className={classes.hero__content__codes}>| C++ | QT |</span>
+          <div className={classes.hero__content__item}>
+            <img
+              className={classes.hero__content__item__img}
+              src={ContainerProj1IMG}
+              width="400px"
+            />
+            <img
+              className={classes.hero__content__item__img}
+              src={ContainerProj2IMG}
+              width="400px"
+            />
+          </div>
+
+          <p className={classes.hero__content__description}>
+            Built with C++ and QT. Its a server program to stack cylinder and
+            box objects onto pallets and send the XML objects over a network to
+            client program, which displays shipping objects in a table model
+            view. This university project required the following techniques.
+            Factory and Momento Design Patterns, XML writing using DOM and
+            XMLStreamer, TCP sockets, Threading, Serialization of class objects,
+            Table model view
+          </p>
+
+          <div className={classes.hero__content__cta}>
+            <a
+              href="https://github.com/Johno4308/Container"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaGithub className={classes.hero__content__cta__item__icon} />}
+              GitHub
+            </a>
+          </div>
+        </div>
+        {/*       GUESTHOUSE           */}
+        <div className={classes.hero__content}>
+          <h1 className={classes.hero__content__title}>GUESTHOUSE</h1>
+          <span className={classes.hero__content__tagline}>
+            Have a few rooms you need to let out, this is the perfect
+            application to do so.
+          </span>
+          <span className={classes.hero__content__codes}>| C++ | QT |</span>
+          <div className={classes.hero__content__item}>
+            <img
+              className={classes.hero__content__item__img}
+              src={GuesthouseProj1IMG}
+              width="400px"
+            />
+            <img
+              className={classes.hero__content__item__img}
+              src={GuesthouseProj2IMG}
+              width="400px"
+            />
+          </div>
+
+          <p className={classes.hero__content__description}>
+            Built with C++ and QT. Its a 5 room BnB booking application that
+            books in guests, checks dates and room availability. This university
+            project required the following techniques. To hard code a UI without
+            using QT onboard designer, Implementation of user input validation,
+            Serialization of class objects, Converting and retrieving data
+            to/from XML.
+          </p>
+
+          <div className={classes.hero__content__cta}>
+            <a
+              href="https://github.com/Johno4308/GuestHouse"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaGithub className={classes.hero__content__cta__item__icon} />}
+              GitHub
+            </a>
+          </div>
         </div>
         <div className={classes.hero__content}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            voluptas ipsam molestiae iste fuga perferendis libero fugit
-            accusantium soluta quidem?
+          <h1 className={classes.hero__content__title}>CALCULATOR</h1>
+          <span className={classes.hero__content__tagline}>
+            TO SUM IT UP.. HERES A SIMPLY CALCULATOR
+          </span>
+          <span className={classes.hero__content__codes}>| C++ | QT |</span>
+          <div className={classes.hero__content__item}>
+            <img
+              className={classes.hero__content__item__img}
+              src={CalculatorProj1IMG}
+              width="80%"
+            />
+          </div>
+
+          <p className={classes.hero__content__description}>
+            Built with C++ and QT. Its a calculator with four basic operations,
+            namely addition, subtraction, multiplication, and division. This
+            university project required the following techniques. To hard code a
+            UI without using QT onboard designer, Implementation of Singleton
+            Design Pattern, Implementation of Factory Design Pattern.
           </p>
-        </div>
-        <div className={classes.hero__content}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            voluptas ipsam molestiae iste fuga perferendis libero fugit
-            accusantium soluta quidem?
-          </p>
-        </div>
-        <div className={classes.hero__content}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            voluptas ipsam molestiae iste fuga perferendis libero fugit
-            accusantium soluta quidem?
-          </p>
-        </div>
-        <div className={classes.hero__content}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            voluptas ipsam molestiae iste fuga perferendis libero fugit
-            accusantium soluta quidem?
-          </p>
-        </div>
-        <div className={classes.hero__content}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
-            voluptas ipsam molestiae iste fuga perferendis libero fugit
-            accusantium soluta quidem?
-          </p>
+
+          <div className={classes.hero__content__cta}>
+            <a
+              href="https://github.com/Johno4308/Calculator"
+              className={classes.hero__content__cta__item}
+            >
+              {<FaGithub className={classes.hero__content__cta__item__icon} />}
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
     </div>
